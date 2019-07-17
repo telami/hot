c07854013b2473863d735dede06c54a33280a723
telami <flyzhu@telami.cn>
2019-07-17 22:20:33 +0800

    [Refactor] change style

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="container">
-        <h3 class="title">互联网热榜</h3>
-        <div class="cat-wrapper">
-            <div class="cat" :class="currentId === item.id ? 'current' : ''" @click="select(item.id)"
-                 v-for="(item,index) in list" :key="index">
-                {{item.title}}
+        <div class="title">互联网热榜</div>
+        <div class="content">
+            <div class="cat-wrapper">
+                <div class="cat" :class="currentId === item.id ? 'current' : ''" @click="select(item.id)"
+                     v-for="(item,index) in list" :key="index">
+                    {{item.title}}
+                </div>
             </div>
-        </div>
-        <div class="info-wrapper">
-            <div class="info" v-for="(info,index) in infos" :key="index">
-                <a :href="info.url" target="_blank">{{info.title}}</a>
+            <div class="info-wrapper">
+                <div class="info" v-for="(info,index) in infos" :key="index">
+                    {{index + 1}}. <a :href="info.url" target="_blank">{{info.title}}</a>
+                </div>
             </div>
         </div>
     </div>
@@ -29,6 +31,7 @@
                 fetch("https://www.printf520.com:8080/GetType").then(response => response.json())
                     .then(data => {
                         let cats = data.Data
+                        cats.pop()
                         cats.pop()
                         this.list = cats
                         this.currentId = this.list[0].id
@@ -60,57 +63,76 @@
     $hot-font-size-bg: 18px;
     $hot-base-color: #FF6F61;
     .container {
+        background-color: #fff;
+
         .title {
+            background-color: #fff;
+            position: fixed;
+            height: 50px;
+            font-size: 20px;
+            line-height: 50px;
             text-align: center;
+            width: 100%;
+            font-weight: bold;
+            z-index: 30;
         }
+        .content {
+            max-width: 960px;
+            margin: 0 auto;
+            padding-top: 60px;
 
-        .cat-wrapper {
-            display: flex;
-            align-items: center;
-            flex-wrap: wrap;
-            justify-content: space-between;
-            font-size: $hot-font-size-bg;
-            margin: 0 50px 15px 50px;
-
-            .cat.current {
-                border-bottom: 2px solid;
-                color: $hot-base-color;
-            }
-        }
-
-        .info-wrapper {
-            text-align: center;
-            margin-bottom: 40px;
-
-            .info {
-                padding: 5px;
-                margin: 5px;
+            .cat-wrapper {
+                display: flex;
+                align-items: center;
+                flex-wrap: wrap;
+                justify-content: space-around;
                 font-size: $hot-font-size-sm;
-
-                a {
-                    text-decoration: none;
+                margin-bottom: 30px;
+                background-color: transparent;
+                border-color: transparent;
+                .cat.current {
+                    background-color: $hot-base-color;
+                    border-radius: 2px;
+                    color: #fff;
                 }
+            }
 
-                /* 未被访问的链接 */
-                a:link {
-                    color: #3c1dff;
-                }
+            .info-wrapper {
+                text-align: left;
+                border: 1px solid #e8e8e8;
 
-                /* 已被访问的链接 */
-                a:visited {
-                    color: #B0C4DE;
-                }
+                .info {
+                    /*text-align: left;*/
+                    padding: 12px 24px;
+                    font-size: $hot-font-size-sm;
+                    border-bottom: 1px solid #e8e8e8;
 
-                /* 鼠标指针移动到链接上 */
-                a:hover {
-                    color: $hot-base-color;
-                }
+                    a {
+                        text-decoration: none;
+                    }
 
-                /* 正在被点击的链接 */
-                a:active {
-                    color: #8B0000;
+                    /* 未被访问的链接 */
+                    a:link {
+                        color: #0A0A0A;
+                    }
+
+                    /* 已被访问的链接 */
+                    a:visited {
+                        color: #808080;
+                    }
+
+                    /* 鼠标指针移动到链接上 */
+                    a:hover {
+                        color: $hot-base-color;
+                    }
+
+                    /* 正在被点击的链接 */
+                    a:active {
+                        color: #8B0000;
+                    }
                 }
             }
         }
+
     }
 </style>
