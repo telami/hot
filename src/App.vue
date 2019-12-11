43756523834d744ae32027fc8b33d5b6e55f035c
telami <907848106@qq.com>
2019-12-11 17:37:45 +0800

    [Refactor] Use new interface and add desc

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,8 @@
 
 <style lang="scss">
     body {
-        background-image: url(assets/bg.png);
-        /*background-color: #d2f0f4;*/
+        /*background-image: url(assets/bg.png);*/
+        /*background-color: rgb(246,246,246);*/
         font-size: 28px;
         /*color: #333333;*/
         font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,189 +1,205 @@
 <template>
-    <div class="container">
-        <h3 class="cuIcon-hotfill text-center">互联网热榜</h3>
-        <loading v-show="show"></loading>
-        <div class="content">
-            <button class="hide" @click="collapse">{{isCollapse ? '展开' : '收起'}}</button>
-            <div ref="cat" class="cat-wrapper flex flex-wrap justify-around">
-                <div class="cat padding-xs radius" :class="currentId === item.id ? 'current' : ''"
-                     @click="select(item.id)"
-                     v-for="(item,index) in list" :key="index">
-                    {{item.title}}
-                </div>
-            </div>
-
-            <div class="info-wrapper shadow bg-white">
-                <div class="info animation-shake padding-tb-sm margin-lr-sm" v-for="(info,index) in infos" :key="index">
-                    {{index + 1}}. <a :href="info.url" target="_blank">{{info.title}}</a>
-                </div>
-            </div>
-            <vueToTop :type="4" :color="'#007BFF'" :size="40"></vueToTop>
+  <div class="container">
+    <h3 class="cuIcon-hotfill text-center">互联网热榜</h3>
+    <loading v-show="show"></loading>
+    <div class="content">
+      <button class="hide" @click="collapse">{{isCollapse ? '展开' : '收起'}}</button>
+      <div ref="cat" class="cat-wrapper flex flex-wrap justify-around">
+        <div class="cat padding-xs radius" :class="currentId === item.id ? 'current' : ''"
+             @click="select(item.id)"
+             v-for="(item,index) in list" :key="index">
+          {{item.title}}
         </div>
-        <a href="https://github.com/telami/hot/issues" class="cuIcon-edit edit"></a>
+      </div>
+
+      <div class="info-wrapper shadow bg-white">
+        <div class="info padding-tb-sm margin-lr-sm" v-for="(info,index) in infos" :key="index">
+          <div class="text-xs text-bold">
+            <a :href="info.Url" target="_blank">{{info.Title}}</a>
+          </div>
+          <div v-if="info.Desc" class="desc padding-top-xs">
+            {{info.Desc}}
+          </div>
+          <div class="text-center padding-top-xs" v-if="info.Url.includes('n.sinaimg.cn')">
+            <img :src="info.Url">
+          </div>
+        </div>
+      </div>
+      <vueToTop :type="4" :color="'#007BFF'" :size="40"></vueToTop>
     </div>
+    <a href="https://github.com/telami/hot/issues" class="cuIcon-edit edit"></a>
+  </div>
 </template>
 <script>
 
-    import Loading from "./loading";
-    import vueToTop from 'vue-totop'
+  import Loading from "./loading";
+  import vueToTop from 'vue-totop'
 
-    export default {
-        components: {Loading, vueToTop},
-        data() {
-            return {
-                theme1: 'light',
-                list: [],
-                infos: [],
-                currentId: 1,
-                show: true,
-                isCollapse: true
-            }
-        },
-        methods: {
-            getAllTypes() {
-                fetch("https://www.tophub.fun:8080/GetType").then(response => response.json())
-                    .then(data => {
-                        this.list = this.filterCats(data.Data);
-                        this.currentId = this.list[0].id
-                        this.show = false
-                    })
-            },
-            getInfo(currentId) {
-                fetch("https://www.tophub.fun:8080/GetTypeInfo?id=" + currentId).then(response => response.json())
-                    .then(data => {
-                        this.infos = this.filterTiebaUrl(data.Data)
-                    })
-            },
-            select(id) {
-                this.currentId = id
-                this.getInfo(id)
-            },
-            filterCats(cats) {
-                let newCats = []
-                for (let cat of cats) {
-                    if (cat.title !== '博客墙' && cat.title !== '反馈建议' && cat.title !== '汇总热榜') {
-                        newCats.push(cat)
-                    }
-                }
-                return newCats.sort(this.compare);
-            },
-            filterTiebaUrl(infos) {
-                let newCats = []
-                for (let info of infos) {
-                    info.url = info.url.replace("amp;", "");
-                    newCats.push(info)
-                }
-                return newCats;
-            },
-            collapse() {
-                this.isCollapse = !this.isCollapse
-                if (this.isCollapse) {
-                    this.$refs.cat.style.height = '45px'
-                } else {
-                    this.$refs.cat.style.height = 'auto'
-                }
-            },
-            compare(o1, o2) {
-                var val1 = o1.sort;
-                var val2 = o2.sort;
-                if (!isNaN(Number(val1)) && !isNaN(Number(val2))) {
-                    val1 = Number(val1);
-                    val2 = Number(val2);
-                }
-                if (val1 > val2) {
-                    return -1;
-                } else if (val1 < val2) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
-        },
-        created() {
-            this.getAllTypes()
-            this.getInfo(this.currentId);
+  export default {
+    components: {Loading, vueToTop},
+    data() {
+      return {
+        theme1: 'light',
+        list: [],
+        infos: [],
+        currentId: 1,
+        show: true,
+        isCollapse: true
+      }
+    },
+    methods: {
+      getAllTypes() {
+        fetch("https://www.tophub.fun:8080/GetType").then(response => response.json())
+          .then(data => {
+            this.list = this.filterCats(data.Data);
+            this.currentId = this.list[0].id
+            this.show = false
+          })
+      },
+      getInfo(currentId) {
+        fetch("https://www.tophub.fun:8888/GetAllInfoGzip?id=" + currentId).then(response => response.json())
+          .then(data => {
+            this.infos = this.filterTiebaUrl(data.Data)
+          })
+      },
+      select(id) {
+        this.currentId = id
+        this.getInfo(id)
+      },
+      filterCats(cats) {
+        let newCats = []
+        for (let cat of cats) {
+          if (cat.title !== '博客墙' && cat.title !== '反馈建议' && cat.title !== '汇总热榜') {
+            newCats.push(cat)
+          }
         }
+        return newCats.sort(this.compare);
+      },
+      filterTiebaUrl(infos) {
+        let newCats = []
+        for (let info of infos) {
+          info.Url = info.Url.replace("amp;", "");
+          newCats.push(info)
+        }
+        return newCats;
+      },
+      collapse() {
+        this.isCollapse = !this.isCollapse
+        if (this.isCollapse) {
+          this.$refs.cat.style.height = '45px'
+        } else {
+          this.$refs.cat.style.height = 'auto'
+        }
+      },
+      compare(o1, o2) {
+        var val1 = o1.sort;
+        var val2 = o2.sort;
+        if (!isNaN(Number(val1)) && !isNaN(Number(val2))) {
+          val1 = Number(val1);
+          val2 = Number(val2);
+        }
+        if (val1 > val2) {
+          return -1;
+        } else if (val1 < val2) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    },
+    created() {
+      this.getAllTypes()
+      this.getInfo(this.currentId);
     }
+  }
 </script>
 
 <style lang="scss">
 
-    @import "../common/main.css";
-    @import "../common/icon.css";
-    @import "../common/animation.css";
+  @import "../common/main.css";
+  @import "../common/icon.css";
+  @import "../common/animation.css";
 
-    $hot-font-size-sm: 15px;
-    $hot-font-size-bg: 18px;
-    $hot-base-color: #007BFF;
-    .container {
+  $hot-font-size-sm: 15px;
+  $hot-font-size-bg: 18px;
+  $hot-base-color: #007BFF;
+  .container {
 
-        .content {
-            max-width: 960px;
-            margin: 0 auto;
+    .content {
+      max-width: 960px;
+      margin: 0 auto;
 
-            .hide{
-                font-size: $hot-font-size-sm;
-                position: absolute;
-                top: 55px;
-            }
+      .hide {
+        font-size: $hot-font-size-sm;
+        position: absolute;
+        top: 55px;
+      }
 
-            .cat-wrapper {
-                font-size: $hot-font-size-sm;
-                margin-bottom: 20px;
-                height: 45px;
-                overflow: hidden;
+      .cat-wrapper {
+        font-size: $hot-font-size-sm;
+        margin-bottom: 20px;
+        height: 45px;
+        overflow: hidden;
 
-                .cat {
-                    cursor: pointer;
-                }
-
-                .cat.current {
-                    background-color: $hot-base-color;
-                    border-radius: 2px;
-                    color: #fff;
-                }
-            }
-
-            .info-wrapper {
-                text-align: left;
-                margin-bottom: 20px;
-
-                .info {
-                    font-size: $hot-font-size-sm;
-                    border-bottom: 1px solid #e8e8e8;
-
-                    a {
-                        text-decoration: none;
-                    }
-
-                    /* 未被访问的链接 */
-                    a:link {
-                        color: #0A0A0A;
-                    }
-
-                    /* 已被访问的链接 */
-                    a:visited {
-                        color: #808080;
-                    }
-
-                    /* 鼠标指针移动到链接上 */
-                    a:hover {
-                        color: $hot-base-color;
-                    }
-
-                    /* 正在被点击的链接 */
-                    a:active {
-                        color: #8B0000;
-                    }
-                }
-            }
+        .cat {
+          cursor: pointer;
         }
 
-        .edit {
-            position: fixed;
-            bottom: 30px;
-            right: 20%;
+        .cat.current {
+          background-color: $hot-base-color;
+          border-radius: 2px;
+          color: #fff;
+        }
+      }
+
+      .info-wrapper {
+        text-align: left;
+        margin-bottom: 20px;
+
+        .info {
+          font-size: $hot-font-size-sm;
+          border-bottom: 1px solid #e8e8e8;
+
+          .desc {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+          }
+
+          a {
             text-decoration: none;
+          }
+
+          /* 未被访问的链接 */
+          a:link {
+            color: #0A0A0A;
+          }
+
+          /* 已被访问的链接 */
+          a:visited {
+            color: #808080;
+          }
+
+          /* 鼠标指针移动到链接上 */
+          a:hover {
+            color: $hot-base-color;
+          }
+
+          /* 正在被点击的链接 */
+          a:active {
+            color: #8B0000;
+          }
         }
+      }
     }
+
+    .edit {
+      position: fixed;
+      bottom: 30px;
+      right: 20%;
+      text-decoration: none;
+    }
+  }
 </style>
